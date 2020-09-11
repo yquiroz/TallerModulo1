b07a35bd66d5d7bf37f4aeee50315b1569833b56
yquiroz <yquiroz@MR0421010006>
2020-09-11 09:32:48 -0500

    Agregar archivos de proyecto.

---
+++ TallerModulo1/Views/Home.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using TallerModulo1.Models.Entidades;
+
+namespace TallerModulo1.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Home : ContentPage
+    {
+
+        public List<LocalesAfiliados> AllLocales;
+
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            CargarLocalesAFiliados();
+
+        }
+
+        public void CargarLocalesAFiliados()
+        {
+            AllLocales = new List<LocalesAfiliados>()
+            {
+                new LocalesAfiliados
+                {
+                    NombreLocalAfiliado = "Wallmart",
+                    ImageUrl = "http://18.218.178.167/imagesemail/walmart.jpg"
+                },
+                new LocalesAfiliados
+                {
+                    NombreLocalAfiliado = "Target",
+                    ImageUrl = "http://18.218.178.167/imagesemail/target.jpg"
+                },
+                new LocalesAfiliados
+                {
+                    NombreLocalAfiliado = "Electrocables",
+                    ImageUrl = "http://18.218.178.167/imagesemail/ferre1.jpg"
+                },
+
+                new LocalesAfiliados
+                {
+                    NombreLocalAfiliado = "FamyAgro",
+                    ImageUrl = "http://18.218.178.167/imagesemail/ferre1.jpg"
+                }
+
+            };
+
+            CLLocales.ItemsSource = AllLocales;
+        }
+
+
+    }
+}
